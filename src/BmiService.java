7087cc9a007e42e7ccb36e3123bdd0adf2898e32
bigrusal <bigrusal@yandex.ru>
2023-04-19 08:17:07 +1000

    code updated

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double highM, double weightKg) {
-        double index = weightKg / (highM*highM);
+        double index = weightKg / (highM * highM);
         int indexBmi = (int) index;
         return indexBmi;
     }
